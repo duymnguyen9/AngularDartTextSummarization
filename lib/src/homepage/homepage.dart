79fe0a1b79ca7c77cba21c29fba449999a034037
Duy <duy.m.nguyen9@gmail.com>
2019-06-05 09:52:09 -0700

    card input layout done, next step is submit button

--- lib/src/homepage/homepage.dart
+++ lib/src/homepage/homepage.dart
@@ -2,13 +2,21 @@ import 'package:angular/angular.dart';
 import 'package:StanfordAngNLP/src/component/stanfordlogo/stanfordlogo.dart';
 import 'package:angular_components/material_input/material_input.dart';
 import 'package:angular_components/material_input/material_input_multiline.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:skawa_material_components/card/card.dart';
 
 @Component(
   selector: 'homepage',
   templateUrl: 'homepage.html',
   directives: [
     StanfordLogoComponent,
-    MaterialMultilineInputComponent
-  ]
+    MaterialMultilineInputComponent,
+    skawaCardDirectives,
+
+  ],
+  styleUrls: [
+    'homepage.scss.css'
+  ],
+  providers: const <dynamic>[materialProviders],
 )
 class HomepageComponent {}
