b6fdbd56b0728f977b2ebd04e16d905b10d3142b
Duy <duy.m.nguyen9@gmail.com>
2019-06-15 17:22:17 -0700

    remove controlpanel folder

--- lib/src/component/component.dart
+++
@@ -1,6 +0,0 @@
-import 'package:StanfordAngNLP/src/component/controlpanel/controlpanel.dart';
-import 'package:StanfordAngNLP/src/component/stanfordlogo/stanfordlogo.dart';
-
-
-export 'package:StanfordAngNLP/src/component/controlpanel/controlpanel.dart';
-export 'package:StanfordAngNLP/src/component/stanfordlogo/stanfordlogo.dart';
